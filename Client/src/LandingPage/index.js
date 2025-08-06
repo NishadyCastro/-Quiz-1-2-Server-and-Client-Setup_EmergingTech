039c0d468708c2be56f0cecc5df739bdfc9892e5
Mariane <ricktakeuchi09@gmail.com>
2025-08-06 23:11:05 +0800

    added LandingPage

---
+++ Client/src/LandingPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Hello World! jk indi ni java WAHAHAHAHAHA</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
